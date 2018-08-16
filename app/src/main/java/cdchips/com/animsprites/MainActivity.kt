8ea2f1d3e3f2593a55977ba26431e0dd5932da60
Carlos Violante Ll <viollams@gmail.com>
2018-08-15 20:37:19 -0500

    v00

--- app/src/main/java/cdchips/com/animsprites/MainActivity.kt
+++ app/src/main/java/cdchips/com/animsprites/MainActivity.kt
@@ -20,9 +20,9 @@ class MainActivity : AppCompatActivity() {
 
     var sprites:ArrayList<Bitmap>? = null
 
-    private val FRAME_W = 88
+    private var FRAME_W = 108
     // frame height
-    private val FRAME_H = 140
+    private var FRAME_H = 140
     // number of frames
     private val NB_FRAMES = 16
     // nb of frames in x
@@ -41,60 +41,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        val birdBmp = getBitmapFromAssets(this, "sprite_run.png")
-
-
-        sprites =  ArrayList<Bitmap>()
-
-        if (birdBmp != null) {
-            // cut bitmaps from bird bmp to array of bitmaps
-            Log.w("Bitmap","h -> ${birdBmp!!.height} , w -> ${birdBmp!!.width}")
-            var currentFrame = 0
-
-            for (i in 0..COUNT_Y-1) {
-                for (j in 0..COUNT_X-1) {
-
-                    Log.w("Bitmap","y -> ${FRAME_H * i} , w -> ${FRAME_H}")
-                    sprites!!.add(Bitmap.createBitmap(birdBmp, FRAME_W
-                        * j, FRAME_H * i, FRAME_W, FRAME_H))
-
-                    /*
-                    sprites!![currentFrame] = Bitmap.createScaledBitmap(
-                            sprites!![currentFrame], FRAME_W / SCALE_FACTOR, FRAME_H / SCALE_FACTOR, true);
-
-*/
-                    if (++currentFrame >= NB_FRAMES) break
-
-                }
-            }
-        }else{
-            Log.w("Main","Bitmap null")
-        }
-
-
-        val animation = AnimationDrawable()
-        animation.isOneShot = false // repeat animation
-
-        for (i in 0..NB_FRAMES-1) {
-            animation.addFrame(BitmapDrawable(resources, sprites!![i]),
-                    FRAME_DURATION)
-        }
-
-        val img = findViewById<ImageView>(R.id.iv_sprite)
-        img.scaleType = ImageView.ScaleType.FIT_CENTER
-        // load animation on image
-        /*
-        if (Build.VERSION.SDK_INT < 16) {
-            img.setBackgroundDrawable(animation)
-        } else {
-            //img.setBackground(animation)
-        }
-        */
-        img.setImageDrawable(animation)
-        img.scaleType = ImageView.ScaleType.FIT_CENTER
-
-        // start animation on image
-        img.post(Runnable { animation.start() })
 
 
     }
